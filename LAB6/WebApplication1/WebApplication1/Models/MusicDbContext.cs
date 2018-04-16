2dabb6bc72a2da2a8e3709485a2bc77ecbded52f
mlynarzsrem <mlynarzsrem@gmail.com>
2018-04-16 16:13:03 +0200

    Lab 6 start

---
+++ LAB6/WebApplication1/WebApplication1/Models/MusicDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WebApplication1.Models{
+    public class MusicDbContext : DbContext{
+        public DbSet<Song> Songs { get; set; }
+        public MusicDbContext() : base(GetOptions("DefaultConnection")) {
+        }
+
+        private static DbContextOptions GetOptions(string connectionString)
+        {
+            return SqlServerDbContextOptionsExtensions.UseSqlServer(new DbContextOptionsBuilder(), connectionString).Options;
+        }
+    }
+}
