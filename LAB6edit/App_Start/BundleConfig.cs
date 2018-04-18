7e311c6a93b1e591a220bf6d12122ac15f5bf57f
mlynarzsrem <mlynarzsrem@gmail.com>
2018-04-18 14:06:29 +0300

    Add files via upload

--- LAB6edit/App_Start/BundleConfig.cs
+++ LAB6edit/App_Start/BundleConfig.cs
@@ -11,8 +11,7 @@ namespace WebApplication5
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.unobtrusive*","~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
