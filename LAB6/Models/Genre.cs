0c8abf49bf4d5e13c301b210e5f1f9da08b5e7e3
mlynarzsrem <mlynarzsrem@gmail.com>
2018-04-20 16:00:24 +0200

    Lab 6 update

---
+++ LAB6/Models/Genre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Genre
+    {
+        public int GenreId { set; get; }
+        public String Name { set; get; }
+        public ICollection<Song> Songs { set; get; }
+        public Genre(int GenreId, String Name)
+        {
+            this.GenreId = GenreId;
+            this.Name = Name;
+        }
+        public Genre() { }
+    }
+}
---
+++ LAB6/Models/MusicDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WebApplication1.Models{
+    public class MusicDbContext : System.Data.Entity.DbContext
+    {
+        public System.Data.Entity.DbSet<Song> Songs { get; set; }
+        public MusicDbContext() : base("DefaultConnection") {
+        }
+
+    }
+}
