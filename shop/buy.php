bf19087c89f5f28b20c5009a7321e9249cd86ef1
mlynarzsrem <mlynarzsrem@gmail.com>
2018-05-07 15:17:42 +0200

    Sho update

--- shop/buy.php
+++ shop/buy.php
@@ -4,18 +4,24 @@
     if(isset($_SESSION['basket'])){
         $basket = $_SESSION['basket'];
         $con->autocommit(FALSE);
+        $isValid =TRUE;
         foreach ($basket as &$product) {
             $result = mysqli_query($con,'SELECT * from products where name like "'.$product.'"') or die("Zapytanie niepoprawne");
             if(mysqli_num_rows($result)!=0){
                 mysqli_query($con,'DELETE from products where name like "'.$product.'"') or die("Zapytanie niepoprawne");
             }
             else{
-                $con->rollback();
-                header('Location: index.php');
+                $isValid =FALSE;
             }
         }
-        $con->commit();
-        unset($_SESSION['basket']);
+        If($isValid ==TRUE){
+            $con->commit();
+            unset($_SESSION['basket']);
+        }
+        else{
+            $con->rollback();
+        }
+        $con->autocommit(TRUE);
     }
     header('Location: index.php');
 ?>
